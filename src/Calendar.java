ca61d9b25d041562e406e21ed3aca1d442db5be7
ghchae <cvxgihyun@gmail.com>
2021-09-28 22:48:19 +0900

    캘린더 요구사항2

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Calendar {
 	public static void main(String[] args) {
@@ -7,5 +8,15 @@ public class Calendar {
 		System.out.println("8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("두 수를 입력해주세요 : ");
+		String inputValue = scan.nextLine();
+		scan.close();
+		String[] values = inputValue.split(" ");
+		int A = Integer.parseInt(values[0]);
+		int B = Integer.parseInt(values[1]);
+		
+		System.out.print(A + B);
 	}
 }
