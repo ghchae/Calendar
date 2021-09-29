503b9c0b9da21d902a63bfbaf136d07815e67f02
ghchae <cvxgihyun@gmail.com>
2021-09-29 20:21:38 +0900

    요일의 시작에따라 달력 출력값을 변경

--- src/Calendar.java
+++ src/Calendar.java
@@ -5,17 +5,39 @@ public class Calendar {
 		if (month == 2 && (year % 4) == 0 && (year % 100) != 0 || year % 400 == 0) return 28;
 		return MAX_DAYS[month - 1];
 	}
-	// static을 사용하는 이유 -> 공부하기, static은 자바파일이 클래스파일로될때부터 인스턴스가 없어도 메모리에 올라가서 사용가능할 수 있다
-	// 지금 main 함수에서 outMaxDays() 메소드를 사용하는데 에러가 나는이유는 
-	// outMaxDays() 메소드를 가진 내가만든 Calendar 클래스의 인스턴스가 없기때문이다.
-	// 따라서 Calendar 인스턴스를 생성해서 메소드를 사용하게하면 static을 사용하지않고 메서드를 사용할 수 있다.
-	
-	public void printCalendar(int maxDay) {
-		System.out.println("일 월 화 수 목 금 토");
+	public int getDayNumber(String day) {
+		switch (day) {
+			case "월" : return 1;
+			case "화" : return 2;
+			case "수" : return 3;
+			case "목" : return 4;
+			case "금" : return 5;
+			case "토" : return 6;
+			case "일" : return 7;
+			default: return 0;
+		}
+	}
+	public void printCalendar(int year,int month, int maxDay, String day) {
+		System.out.printf("<<%d년 %d월 %s요일 시작 >>\n", year, month, day);
+		System.out.println("일   월   화   수   목   금   토");
 		System.out.println("--------------------");
-		for(int i = 1; i <= maxDay; i++) {
+		for (int j = 1; j <= getDayNumber(day); j++) {
+			System.out.print("   ");
+		}
+		for (int t = 1; t <= (7-getDayNumber(day)); t++) {
+			if (t < 10) {
+				System.out.print(" ");
+			}
+			System.out.print(t + " ");
+		}
+		System.out.println();
+		
+		for (int i = 8-getDayNumber(day); i <= maxDay; i++) {
+			if (i < 10) {
+				System.out.print(" ");
+			}
 			System.out.print(i + " ");
-			if (i % 7 == 0) System.out.println();
+			if (i % 7 == 7-getDayNumber(day)) System.out.println();
 		}
 		System.out.println();
 	}
--- src/Prompt.java
+++ src/Prompt.java
@@ -4,10 +4,10 @@ public class Prompt {
 	public static void main(String[] args) {
 		Calendar cal = new Calendar();
 		Scanner scan = new Scanner(System.in);
-		// 년, 월, 요일을 입력 받기 위해서
 		
 		int year = 0;
 		int month = 0;
+		String day = "월";
 		int maxDay = 0;
 		while(true) {
 			System.out.print("YEAR >");
@@ -25,9 +25,15 @@ public class Prompt {
 				System.out.println("잘못입력하셨습니다. 1 ~ 12 월 까지만 입력해주세요.");
 				continue;
 			}
+			System.out.print("DAY >");
+			day = scan.next();
 			maxDay = cal.getMaxDays(year, month);
+			if (cal.getDayNumber(day) == 0) {
+				System.out.println("잘못된 입력입니다.");
+				continue;
+			}
 			System.out.printf("%d년의 %d월의 마지막 일은 %d입니다.\n\n",year, month, maxDay);
-			cal.printCalendar(maxDay);
+			cal.printCalendar(year,month, maxDay, day);
 		}
 		scan.close();
 	}
